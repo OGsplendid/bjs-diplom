09e4859acdaa3c09a1e5d2a529eb4ab680e89e50
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-11-14 19:36:47 +0300

    Update homePage.js

---
+++ public/homePage.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const logoutButton = new LogoutButton();
+
+logoutButton.action = logout => {
+    ApiConnector.logout(response => {
+        if (response) {
+            location.reload();
+        }
+    });
+}
+
+ApiConnector.current(response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
+const ratesBoard = new RatesBoard();
+
+function ratesRequest() {
+    ApiConnector.getStocks(response => {
+        console.log(response);
+        if (response.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+        }
+    })
+};
+
+setInterval(ratesRequest, 60000);
