d1959e4b0c9652a865b179a27cbb07c0d34eab0b
OGsplendid <108668584+OGsplendid@users.noreply.github.com>
2022-11-16 20:50:28 +0300

    Update loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -4,24 +4,8 @@ const userform = new UserForm();
 
 userform.loginFormCallback = data => {
     ApiConnector.login(data, response => response.success ? location.reload() : userform.setLoginErrorMessage(response.error));
-    // {
-    //     response.success ? location.reload() : userform.setLoginErrorMessage(response.error);
-        // if (response.success) {
-        //     location.reload();
-        // } else {
-        //     userform.setLoginErrorMessage(response.error);
-        // }
-    // });
 }
 
 userform.registerFormCallback = data => {
     ApiConnector.register(data, response => response.success ? location.reload() : userform.setRegisterErrorMessage(response.error));
-    // {
-    //     response.success ? location.reload() : userform.setRegisterErrorMessage(response.error); 
-        // if (response.success) {
-        //     location.reload();
-        // } else {
-        //     userform.setRegisterErrorMessage(response.error);
-        // }
-    // });
 }
